9b2bcffd2e4e464d2a657c59bfc37aca632e244c
Clint Carafelli <clintcarafelli@icloud.com>
2026-05-10 14:33:15 -0600

    add base sensor template and driver registry

---
+++ BaseSensor.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+class Reading: 
+    # DESCRIPTION: solves the issue of trying to print a class.
+    # instead of printing out a memory address, this will
+    # return the measurment name, value, and unit 
+    def __init__(self, name, value, unit) -> None:
+        self.name = name
+        self.value = value
+        self.unit = unit
+
+    def __repr__(self) -> str:
+        return f"{self.name}: {self.value} {self.unit}"
+
+class BaseSensor(ABC):
+
+    def __init__(self, sensor_id: str, config_dict: dict, i2c_bus: any) -> None: 
+        self.sensor_id = sensor_id
+        self.config_dict = config_dict
+        self.description = config_dict.get("description", sensor_id)
+        self.readings_meta_data: list[dict] = config_dict.get("readings", [])
+        self.i2c_bus = i2c_bus
+        self.initialized = False
+
+    @abstractmethod
+    def initialize(self) -> None :
+        """ Use this base method to initialize sensors"""
+
+
+    @abstractmethod
+    def read(self) -> list[Reading]:
+        """ Use this base method to initialize sensors"""
+
+
+from SCD30Driver import SCD30Driver
+from BMP388Driver import BMP388Driver
+from STEMMADriver import STEMMADriver
+from ME2Driver import ME2Driver
+from INA260Driver import INA260Driver
+from SensorErrorSolutions import TransientSolution, PersistentSolution, PowerCycleSolution
+
+# Map the class that drives the sensor to the driver field in the config.toml file
+driver_registry: dict = {
+    "SCD30_driver":   SCD30Driver,
+    "BMP388_driver":  BMP388Driver,
+    "STEMMA1_driver": STEMMADriver,
+    "STEMMA2_driver": STEMMADriver,
+    "STEMMA3_driver": STEMMADriver,
+    "ME2_driver":     ME2Driver,
+    "INA260_driver":  INA260Driver,
+}
+ 
+solution_registry: dict = {
+    "transient_solution": TransientSolution,
+    "persistent_solution": PersistentSolution,
+    "power_cycle_solution": PowerCycleSolution
+ }
+
